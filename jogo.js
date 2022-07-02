2f12c1ad8d0158c95d0be85296b20b28920597de
Bozzi <74721938+Teteuzins2@users.noreply.github.com>
2022-07-02 16:53:27 -0300

    Create jogo.js

---
+++ jogo.js
@@ -0,0 +1,7 @@
+console.log('[DevSoutinho] Flappy Bird');
+
+const sprites = new Image();
+sprites.src = './sprites.png';
+
+const canvas = document.querySelector('canvas');
+const contexto = canvas.getContext('2d');
