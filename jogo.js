950293c6db4a28f1e30aa8e892bc6ba3d8ba5552
Bozzi <mateuspvz1@gmail.com>
2022-07-02 21:12:13 -0300

    upload

--- jogo.js
+++ jogo.js
@@ -5,3 +5,16 @@ sprites.src = './sprites.png';
 
 const canvas = document.querySelector('canvas');
 const contexto = canvas.getContext('2d');
+
+
+function loop () {
+    contexto.drawImage(
+        sprites,
+        0, 0,   // Sprite X , Sprite Y
+        34, 24, // Tamanho do recorte na sprite
+        10, 10, // Posicionamento na tela
+        34, 24 // Tamanho da imagem
+    );
+
+    requestAnimationFrame(loop);
+}
